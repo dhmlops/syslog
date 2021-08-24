a2cc480450c20112196c0d580832045bfb250588
jax79sg <jax79sg@yahoo.com.sg>
2021-08-24 20:30:54 +0800

    centralised

---
+++ centralised/createcentralsyslog.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+sudo apt update
+sudo apt install -y wget rsyslog
+wget 
+sudo cp rsyslog.conf /etc/
+sudo systemctl start rsyslog
+sudo systemctl enable rsyslog
+sudo ufw allow 514/tcp
+sudo ufw allow 514/udp
+sudo systemctl restart rsyslog
+sudo ss -tulnp | grep "rsyslog"
+
--- createcentralsyslog.sh
+++
@@ -1,5 +0,0 @@
-
-#!/bin/bash
-sudo apt install -y rsyslog
-sudo systemctl start rsyslog
-sudo systemctl enable rsyslog
