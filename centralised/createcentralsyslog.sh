d5f29f05f605dbf6720220d5bee39720ed1231dd
jax79sg <jax79sg@yahoo.com.sg>
2021-08-24 23:24:30 +0800

    script update

--- centralised/createcentralsyslog.sh
+++ centralised/createcentralsyslog.sh
@@ -2,12 +2,12 @@
 #!/bin/bash
 sudo apt update
 sudo apt install -y wget rsyslog
-wget https://raw.githubusercontent.com/jax79sg/syslog/main/centralised/rsyslog.conf 
+#wget https://raw.githubusercontent.com/jax79sg/syslog/main/centralised/rsyslog.conf 
 sudo cp rsyslog.conf /etc/
 sudo systemctl start rsyslog
 sudo systemctl enable rsyslog
 sudo ufw allow 514/tcp
-sudo ufw allow 514/udp
+#sudo ufw allow 514/udp
 sudo systemctl restart rsyslog
 sudo ss -tulnp | grep "rsyslog"
 
